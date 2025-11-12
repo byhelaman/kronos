81aada15c605bff1b2ac590ede5c860bed38519e
byhelaman <byhelaman@gmail.com>
2025-11-11 23:07:24 -0500

    fix policy css

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ class SecurityHeadersMiddleware(BaseHTTPMiddleware):
         response.headers["Content-Security-Policy"] = (
             "default-src 'self'; "
             "script-src 'self' 'unsafe-inline'; "
-            "style-src 'self' 'unsafe-inline'; https://fonts.googleapis.com; "
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
             "font-src 'self' https://fonts.gstatic.com; "
             "connect-src 'self'; "
             "form-action 'self'; "
